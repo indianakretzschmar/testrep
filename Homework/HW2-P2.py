60fd302778757bbbb8affafb0d24beb202ba4604
Indiana Kretzschmar <indiana-kretzschumar@Indianas-MacBook-Pro.local>
2024-09-13 22:04:19 -0600

    Final commit HW4

--- Homework/HW2-P2.py
+++ Homework/HW2-P2.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 A = 0.5 * np.array([[1, 1], [1 + 10e-10, 1 - 10e-10]])
-
+B = np.array([[1],[1]])
+norm_b = np.linalg.norm(B)
 # Calculate the norm of A
 norm_A = np.linalg.norm(A)
 
@@ -13,7 +14,8 @@ norm_A_inv = np.linalg.norm(A_inv)
 
 # Calculate the condition number using the formula
 condition_number_formula = norm_A * norm_A_inv
+print(norm_b)
 
-print(norm_A)
-print(norm_A_inv)
-print(condition_number_formula)
+# print(norm_A)
+# print(norm_A_inv)
+# print(condition_number_formula)
